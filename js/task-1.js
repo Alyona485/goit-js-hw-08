66161c939fa28f5a32f117df1ee894f28dddca75
alyona <akovalcuk485@gmail.com>
2024-08-15 17:05:14 +0300

    finish markup

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const list = document.querySelector('#categories');
-console.log(`Number of categories: ${list.children.length}`);
-const item = document.querySelectorAll('.item');
-item.forEach(element => {
-  console.log(`Category: ${element.firstElementChild.textContent}`);
-  console.log(`Elements: ${element.lastElementChild.children.length}`);
-});
